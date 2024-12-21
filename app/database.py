ccfd41d2a03da3c9f343ca73e6e4d5e2d9c328b2
Amira Fathalla <amiraxamira@pc-113.home>
2024-12-21 19:16:00 +0100

    modif database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,26 @@
+#backend/app/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
+#URL de connexion à la bdd
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+#création de l'engine
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
+
+#session de la bdd
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#création des tables
+#création des tables si elles n'existent pas
 Base.metadata.create_all(bind=engine)
+
+#dépendance pour obtenir session de bdd
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
